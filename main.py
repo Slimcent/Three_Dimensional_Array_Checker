63b3e2eb31af8495ad1c8855eea539ddc8d43545
Slimcent <centpresley@gmail.com>
2023-08-01 18:27:36 +0100

    Three dimensional array function

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+def is_three_dimensional_array(arr):
+    if isinstance(arr, list) and len(arr) > 0:
+        if all(isinstance(level, list) for level in arr):
+            if all(isinstance(row, list) for level in arr for row in level):
+                # Additional check: Ensure that all levels have the same number of rows and columns
+                level_shape = set((len(level), len(level[0])) for level in arr)
+                if len(level_shape) == 1:
+                    return True
+    return False
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+three_dim_array = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+two_dim_array = [[1, 2, 3], [4, 5, 6]]
+not_array = 42
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(is_three_dimensional_array(two_dim_array))
+print()
+print(is_three_dimensional_array(three_dim_array))
+print()
+print(is_three_dimensional_array(not_array))
